ec92f6ae3dd34763042acb6161a7948cc6d4b61e
LeticiaGSoares <leticiag.soares.dev@gmail.com>
2024-08-28 09:13:13 -0300

    :sparkles: feat: Função de buscar tarefa por id completa

--- src/controllers/tarefaControllers.js
+++ src/controllers/tarefaControllers.js
@@ -1,17 +1,16 @@
 import Tarefa from "../models/tarefaModel.js"
 
-export const getAll = async (req, res) => {
-    try{
-        const tarefas = await Tarefa.findAll()
-        res.status(200).json(tarefas)
-    } catch(error){
-        res.status(500).json({message: "Erro ao listar tarefas"})
-    }
-}
-
+// export const getAll = async (req, res) => {
+//     try{
+//         const tarefas = await Tarefa.findAll()
+//         res.status(200).json(tarefas)
+//     } catch(error){
+//         res.status(500).json({message: "Erro ao listar tarefas"})
+//     }
+// }
 export const create = async (req, res) => {
     const {tarefa, descricao} = req.body
-    const status = "pedente"
+    const status = 1
 
     if(!tarefa){
         res.status(400).json({validationError: "A tarefa é obrigatória"})
@@ -32,5 +31,47 @@ export const create = async (req, res) => {
     }catch (error){
         console.error(error)
         res.status(500).json({message: "Erro interno do servidor"})
+    }
+}
+
+//tarefas?page=3&limit=10
+export const getAll = async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = 3
+    const offset = (page - 1) * limit
+    try{
+        const tarefas = await Tarefa.findAndCountAll({
+            limit,
+            offset
+        })
+
+        const totalPaginas = Math.ceil(tarefas.count / limit)
+        res.status(200).json({
+            totalTarefas: tarefas.count,
+            totalPags: totalPaginas,
+            pagAtual: page,
+            itensPorPag: limit,
+            ProximaPag: totalPaginas === 0 ? null : `http://localhost:3333/tarefas?page=${page + 1}`,
+            pagAnterior: page - 1 === 0 ? null : `http://localhost:3333/tarefas?page=${page - 1}`,
+            tarefas: tarefas.rows
+        });
+    }catch(error){
+        res.status(500).json({message: "Erro interno do servidor: "+ error})
+    }
+}
+
+export const getTarefa = async (req, res) => {
+    const {tarefa_id} = req.params
+
+    try{
+        const tarefa = await Tarefa.findByPk(tarefa_id)
+
+        if (!tarefa) {
+            return res.status(404).json({message: `Tarefa ${tarefa_id} não existe` });
+        } 
+
+        res.status(200).json({message: tarefa})
+    }catch (error){
+        res.status(500).json({message: "Erro interno do servidor" + error})
     }
 }
--- src/routes/tarefaRouter.js
+++ src/routes/tarefaRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getAll, create } from "../controllers/tarefaControllers.js"
+import { getAll, create, getTarefa } from "../controllers/tarefaControllers.js"
 
 const router = Router()
 
 router.get("/", getAll)
 router.post("/", create)
+router.get("/:tarefa_id", getTarefa)
 
 export default router;
